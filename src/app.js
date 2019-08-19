5281ff0c91e16e0c325d4874f8162d345ba356f2
Wijnand Korlaar <korlaarwijnand@hotmail.com>
2019-08-19 18:39:39 +0200

    Settup for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicPath = path.join(__dirname, "../public");
@@ -80,6 +81,6 @@ app.get("*", (req, res) => {
     error: "something went wrong"
   });
 });
-app.listen(3000, () => {
-  console.log("Running on 3000");
+app.listen(port, () => {
+  console.log(`server on ${port}`);
 });
